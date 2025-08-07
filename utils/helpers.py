4c21cbe9bc8fac9add3ecddb5e68123793a55ec0
ysb74 <76511755+ysb74@users.noreply.github.com>
2025-08-07 18:01:29 +0000

    helper function

---
+++ utils/helpers.py
@@ -0,0 +1,8 @@
+def download_file(url, target_path):
+    import requests
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(target_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return target_path
